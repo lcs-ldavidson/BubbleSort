3321f6737c59d29d4568047fab3b24116a83c251
Liam Davidson <ldavidson@lcs.on.ca>
2020-05-06 14:42:16 -0400

    Created an unoptimized bubble sort.

--- BubbleSort/main.swift
+++ BubbleSort/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+//create an empty array
+var dataSet : [Int] = []
+
+//populate the array to sort
+for _ in 1...10 {
+    dataSet.append(Int.random(in: 1...100))
+}
+
+//print unsorted array
+print(dataSet)
+
+for i in 1...dataSet.count {
+    
+    //one pass through array to float highest number to the end
+    for j in 0...dataSet.count - 2 {
+        
+        //compare left value to right value
+        if dataSet[j] > dataSet[j+1] {
+            let temporaryValue = dataSet[j]
+            dataSet[j] = dataSet[j+1]
+            dataSet[j+1] = temporaryValue
+            
+        }
+        //print the array after the pass through
+        print(dataSet)
+    }
+    //print the array after the pass through
+    print(dataSet)
+}
